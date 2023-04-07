59f564b0d47fd678ddf9eb0c4b1f594489a52ed3
gitGUzz <null@nullNaN.com>
2023-04-07 09:56:35 +0300

    the key to seperation in arrays

--- src/arrayinput.java
+++ src/arrayinput.java
@@ -1,15 +1,19 @@
+import java.util.Arrays;
 import java.util.Scanner;
-
+/*
+    TODO:
+        > scan an input of "xx:yy" and separate the ':'
+        >  xx:yy must be
+        > [0 1 2], where ':' is "1" and we calculate the values of "0" and 2"
+ */
 public class arrayinput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String time1 = sc.nextLine();
 
-        String[] values_1 = new String[1];
-        values_1 = time1.split(",");
+        String[] arrayTimeValues_1 = time1.split(":");
 
-        for (int i = 0; i < values_1.length; i++) {
-            System.out.println(time1);
-        }
+        System.out.println(Arrays.toString(arrayTimeValues_1));
+        System.out.println(arrayTimeValues_1.length);
     }
 }
